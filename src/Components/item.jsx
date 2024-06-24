72fbc1557e59eca20c1b8b0beee5b7753eeb689a
Talya2004 <talya3259@gmail.com>
2024-06-24 12:29:06 +0300

    Responsive component

--- src/Components/item.jsx
+++ src/Components/item.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import '../Item1.css'; // קובץ סגנון חיצוני לקומפוננטה Item
 
 function Item({ company, nameTender, datePublished, dateSubmission, category, winnerDetails, offer, winnerData, bidAmount, id }) {
@@ -10,7 +10,7 @@ function Item({ company, nameTender, datePublished, dateSubmission, category, wi
                         <th>שם הגוף</th>
                         <th>שם ומספר המכרז</th>
                         <th>תאריך פרסום</th>
-                        <th>תאריך פרסום</th>
+                        <th>תאריך הגשה</th>
                         <th>קטגוריות</th>
                         <th>שם הזוכה ופרטי</th>
                         <th>מציעים</th>
@@ -21,16 +21,16 @@ function Item({ company, nameTender, datePublished, dateSubmission, category, wi
                 </thead>
                 <tbody>
                     <tr>
-                        <td>{company}</td>
-                        <td>{nameTender}</td>
-                        <td>{datePublished}</td>
-                        <td>{dateSubmission}</td>
-                        <td>{category.join(', ')}</td>
-                        <td>{winnerDetails}</td>
-                        <td>{offer}</td>
-                        <td>{winnerData}</td>
-                        <td>{bidAmount}</td>
-                        <td>{id}</td>
+                        <td data-label="שם הגוף">{company}</td>
+                        <td data-label="שם ומספר המכרז">{nameTender}</td>
+                        <td data-label="תאריך פרסום">{datePublished}</td>
+                        <td data-label="תאריך הגשה">{dateSubmission}</td>
+                        <td data-label="קטגוריות">{category.join(', ')}</td>
+                        <td data-label="שם הזוכה ופרטי">{winnerDetails}</td>
+                        <td data-label="מציעים">{offer}</td>
+                        <td data-label="מידע על הזוכה">{winnerData}</td>
+                        <td data-label="סכום ההצעה">{bidAmount}</td>
+                        <td data-label="ID">{id}</td>
                     </tr>
                 </tbody>
             </table>
@@ -39,4 +39,3 @@ function Item({ company, nameTender, datePublished, dateSubmission, category, wi
 }
 
 export default Item;
-
