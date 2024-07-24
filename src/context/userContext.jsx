a1cf0b99fdbfa7a9fb740d85fa2c26bd04cf5e1b
sari-itzkovch <sarit2813@gmail.com>
2024-07-24 11:25:38 +0300

    user

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({role:'admin'});
+  const [user, setUser] = useState({role:'user'});
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
