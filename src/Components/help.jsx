d0983104f08c5a069dc77ae91b997e05c97d34b0
rutnotoviz <r0504172131@gmail.com>
2024-07-31 11:58:47 +0300

    Updated SearchTenders component

--- src/Components/help.jsx
+++ src/Components/help.jsx
@@ -1,36 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Help= () => {
-console.log("fyug");
-    return (
-        <div>
-          
-            <h1>מסך עזרה ותמיכה-משתמשים יכולים לגשת לצאט לקבל עזרה בעזרת רובוט וטופס ליצירת קשר ממלאים פרטים ויוצרים קשר...</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error veniam soluta eius quis maxime sit ut veritatis aliquid repellendus nam, incidunt culpa, quas recusandae ipsam cum deserunt in assumenda. Ad.</p>
 
-         </div>
-    );
-}
+const help = () => {
 
-export default Help;
+  
+};
+
+export default help;
+
+
+
+
--- src/Components/resultTender.jsx
+++ src/Components/resultTender.jsx
@@ -1,48 +1,138 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './result.css'; 
 
-const ResultTender= () => {
-    return (
-        <div>
-       
-            <h1>תוצאות מכרזים שונים</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque maiores ullam maxime totam voluptatum magnam dolorum quia? Soluta minus, illum necessitatibus rerum in totam enim est sed maiores optio dolore.</p>
+const SearchTenders = () => {
+  const [filters, setFilters] = useState({
+    category: '',
+    tender_name: '',
+    startDate: '',
+    endDate: '',
+    participant_name: '',
+  });
 
+  const [tenders, setTenders] = useState([]); // State to store fetched tenders
+  const [filteredTenders, setFilteredTenders] = useState([]); // State to store filtered tenders
+  const [hasSearched, setHasSearched] = useState(false);
+  const [loading, setLoading] = useState(true); // State to handle loading
+
+  useEffect(() => {
+    const fetchTenders = async () => {
+      try {
+        setLoading(true); // Set loading to true before fetching
+        const response = await fetch('API_ENDPOINT'); // Replace with your API endpoint
+        const data = await response.json();
+        setTenders(data); // Set the fetched tenders
+        setFilteredTenders(data); // Initialize filtered tenders with all fetched tenders
+      } catch (error) {
+        console.error('Error fetching tenders:', error);
+      } finally {
+        setLoading(false); // Set loading to false after fetching
+      }
+    };
+
+    fetchTenders();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFilters({ ...filters, [name]: value });
+  };
+
+  const handleSearch = () => {
+    const filtered = tenders.filter(tender => {
+      const tenderDate = new Date(tender.date);
+      const startDate = new Date(filters.startDate);
+      const endDate = new Date(filters.endDate);
+
+      return (
+        (filters.category === '' || tender.category.includes(filters.category)) &&
+        (filters.tender_name === '' || tender.name.includes(filters.tender_name)) &&
+        (filters.startDate === '' || tenderDate >= startDate) && // Check if tender date is after or on the start date
+        (filters.endDate === '' || tenderDate <= endDate) && // Check if tender date is before or on the end date
+        (filters.participant_name === '' || tender.participant.includes(filters.participant_name))
+      );
+    });
+
+    setFilteredTenders(filtered);
+    setHasSearched(true);
+  };
+
+  return (
+    <div className="tender-filter-container">
+      <h2>חיפוש מכרזים</h2>
+      <div className="filter-inputs">
+        <div className="input-group">
+          <label className="label-category">קטגוריה</label>
+          <input
+            type="text"
+            name="category"
+            placeholder="Enter category"
+            value={filters.category}
+            onChange={handleChange}
+          />
         </div>
-    );
-}
+        <div className="input-group">
+          <label className="label-category">שם מכרז</label>
+          <input
+            type="text"
+            name="tender_name"
+            placeholder="Enter tender name"
+            value={filters.tender_name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-group">
+          <label className="label-category">תאריך התחלה</label>
+          <input
+            type="date"
+            name="startDate"
+            value={filters.startDate}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-group">
+          <label className="label-category">תאריך סיום</label>
+          <input
+            type="date"
+            name="endDate"
+            value={filters.endDate}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="input-group">
+          <label className="label-category">שם משתתף</label>
+          <input
+            type="text"
+            name="participant_name"
+            placeholder="Enter participant name"
+            value={filters.participant_name}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <button onClick={handleSearch} className="search-button">חיפוש</button>
+      <div className="tender-list">
+        {loading ? (
+          <p>טוען מכרזים...</p> // Loading message
+        ) : hasSearched && filteredTenders.length > 0 ? (
+          filteredTenders.map((tender) => (
+            <div key={tender.id} className="tender-item">
+              <p><strong>שם:</strong> {tender.name}</p>
+              <p><strong>קטגוריה:</strong> {tender.category}</p>
+              <p><strong>תאריך:</strong> {tender.date}</p>
+              <p><strong>משתתף:</strong> {tender.participant}</p>
+            </div>
+          ))
+        ) : (
+          hasSearched && <p>אין תוצאות להצגה. אנא צמצם את החיפוש שלך.</p>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default ResultTender ;
+export default SearchTenders;
+
+
+
+
